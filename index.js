8fe1f81728cb95c4cd65c5628d76ced1147c8cff
Alex Simisi Nta <alexsms.devops@gmail.com>
2023-07-25 21:22:17 +0200

    update .gitignore and server file

--- index.js
+++ index.js
@@ -2,20 +2,39 @@ const express = require("express");
 const colors = require("colors");
 const mongoose = require("mongoose");
 const Product = require("./models/productModel");
+const password = require("./password.json")
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 // routes
-app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/blog", (req, res) => res.send("Hello Blog!"));
+/* GET all products */
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+/* GET product by Id */
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+/* POST create product */
 app.post("/product", async (req, res) => {
   try {
-    const product = await Product.create(req.body)
-    res.status(200).json(product)
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
@@ -25,7 +44,7 @@ app.post("/product", async (req, res) => {
 // connection
 mongoose
   .connect(
-    "mongodb+srv://admin:Admin@devrestapi.8rky0xg.mongodb.net/Node-API?retryWrites=true&w=majority"
+    `mongodb+srv://admin:${password.password}@devrestapi.8rky0xg.mongodb.net/Node-API?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(port, () =>
