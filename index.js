ee439319878fe87c8df4bb6798c59c686bd5fbd0
Alex Simisi Nta <alexsms.devops@gmail.com>
2023-07-25 19:58:42 +0200

    create product model

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// routes
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/blog", (req, res) => res.send("Hello Blog!"));
 
+app.post("/product", (req, res) => {
+  try {
+    console.log(req.body);
+    res.send(req.body);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// connection
 mongoose
   .connect(
     "mongodb+srv://admin:<password>@devrestapi.8rky0xg.mongodb.net/Node-API?retryWrites=true&w=majority"
