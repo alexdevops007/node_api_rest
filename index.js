78a9d5140764df49cef2990d56077a031c42b98f
Alex Simisi Nta <alexsms.devops@gmail.com>
2023-07-25 03:41:08 +0200

    create server file index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const colors = require("colors");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Server up and running on port ${port}`.green.underline.bold)
+);
